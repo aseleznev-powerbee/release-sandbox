286cb7a009100b0852f5a52e8ce7912d5dcabe75
Aleksey Seleznev <aseleznev@powbee.ru>
2026-02-02 17:28:30 +0300

    sprint 1 task 1 fix

--- file1.php
+++ file1.php
@@ -1,6 +1,6 @@
 <?php
 echo "file1.php content" . PHP_EOL;
-echo "sprint 1 task 1 content" . PHP_EOL;
+echo "sprint 1 task 1 fixed content" . PHP_EOL;
 echo "sprint 1 task 2 another fix content" . PHP_EOL;
 echo "sprint 1 task 3 content" . PHP_EOL;
 echo "sprint 2 task 1 content" . PHP_EOL;
